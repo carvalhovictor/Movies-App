cdd3ace3da7d9d4e287274f8fb78c29f51cc2798
Victor Carvalho <victorcamargocarvalho@gmail.com>
2020-04-27 16:21:23 -0300

    implemented login functionality

--- js/app.js
+++ js/app.js
@@ -1,8 +1,22 @@
 angular.module("moviesApp", []);
 
-angular.module("moviesApp").controller("registerCtrl", function($scope){
+angular.module("moviesApp").controller("registerCtrl", function($scope, $http){
 	$scope.app = "Movies";
-	$scope.register = function(user){
-		console.log(user);
+
+	$scope.register = function(){
+	var config = {
+		method : 'POST',
+		url : 'http://localhost:8888/movies/login.php',
+		data : {
+			'username' : $scope.user.email,
+			'password' : $scope.user.password
+		}
 	};
+	var request = $http(config);
+	request.then(function(response){
+		window.location.href = 'mymovies.html';
+	},function(error){
+		console.log(error.data);
+	});
+};
 });
