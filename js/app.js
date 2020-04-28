a35288742a5fc74d1393b5c10e1bb67e2d4d7576
Victor Carvalho <victorcamargocarvalho@gmail.com>
2020-04-28 16:25:42 -0300

    implemented search for movies from API

--- js/app.js
+++ js/app.js
@@ -9,7 +9,9 @@ angular.module("moviesApp").controller("registerCtrl", function($scope, $http){
 		url : 'http://localhost:8888/movies/register.php',
 		data : {
 			'username' : $scope.user.email,
+			'alias' : $scope.user.alias,
 			'password' : $scope.user.password
+			
 		}
 	};
 	var request = $http(config);
@@ -22,10 +24,11 @@ angular.module("moviesApp").controller("registerCtrl", function($scope, $http){
 });
 
 angular.module("moviesApp").controller("loginCtrl", function($scope, $http){
-
+	$scope.msg = "";
 	$scope.submit = function(){
 	var config = {
 		method : 'POST',
+		crossDomain : true,
 		url : 'http://localhost:8888/movies/login.php',
 		data : {
 			'username' : $scope.user.email,
@@ -35,12 +38,37 @@ angular.module("moviesApp").controller("loginCtrl", function($scope, $http){
 	var request = $http(config);
 	request.then(function(response){
 		if(response.data == 1){
-			console.log(response.data);
+			window.location.href = 'mymovies.html';
 		}else{
-			console.log(response.data);
+			$scope.msg = response.data;
 		}	
 	},function(error){
 		console.log(error.data);
 	});
 };
+});
+
+angular.module("moviesApp").controller("moviesCtrl", function($scope, $http){
+	$scope.movie = [];
+	$scope.flag = false;
+	//$scope.user = '';
+
+	$scope.loadMovie = function(title){
+		$http.get('http://www.omdbapi.com/?t='+title+'&apikey=1ebdc3a0&plot=short').then(function(response){
+			$scope.movie = response.data;
+			$scope.flag = true;
+			delete $scope.msg;
+			if(response.data.Response == 'False'){
+				$scope.flag = false;
+				$scope.msg = response.data.Error
+			}
+			delete $scope.search;
+		});
+	};
+
+	/*$http.get('http://localhost:8888/movies/getSession.php').then(function(response){
+		console.log(response.data);
+		$scope.user = response.data;
+	});
+	*/
 });
